532fd1904616cfd7938a3b154649466abae12a41
aptalca <aptalca@users.noreply.github.com>
2019-05-13 16:55:17 -0400

    Create sync.sh

---
+++ root/app/sync.sh
@@ -0,0 +1,6 @@
+echo "***********************************"
+echo "*                                 *"
+echo "*                                 *"
+echo "***********************************"
+echo $(date)
+echo "Enter your sync lines here"
